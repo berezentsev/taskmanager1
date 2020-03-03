a670b8696a440bcf7587af7aa6e76f263bf713bd
Evgeny <berezentsev.e@mail.ru>
2020-03-03 19:37:06 +0400

    накатана миграция по созданию таблицы tasks

---
+++ database/migrations/2020_03_03_153113_create_table_tasks.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableTasks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('uuid');
+            $table->string('title');
+            $table->integer('priority')->unsigned()->default(1);
+            $table->json('tags');
+            $table->integer('status')->unsigned()->default(1);
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
